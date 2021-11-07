4e04b0ac61e84e0c9621fd756a83d4e029a21e71
Jocimar B. A. Huss <jocimarspi@gmail.com>
2021-11-07 19:57:48 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import ImgDsDark from "assets/img/ds-dark.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por{" "}
+          <a
+            href="https://github.com/jocimarspi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jocimar B. A. Huss
+          </a>
+        </p>
+        <p className="text-light">
+          <small>
+            <strong>Semana Spring React</strong>
+            <br />
+            Evento promovido pela escola DevSuperior:{" "}
+            <a
+              href="https://instagram.com/devsuperior.ig"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @devsuperior.ig
+            </a>
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
